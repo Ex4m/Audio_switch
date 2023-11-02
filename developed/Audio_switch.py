2d6e1e032acdc903a8b631d86e09c9cfa5e4ae3a
Ex4m <121984532+Ex4m@users.noreply.github.com>
2023-11-02 23:43:42 +0100

    WIP
    - hotkeys WIP
    - autostart with pythonw
    - restructure of the project

---
+++ developed/Audio_switch.py
@@ -0,0 +1,95 @@
+import subprocess as sb
+import os
+import keyboard
+from Engine import SetupStuff
+import time
+import sys
+
+e = SetupStuff()
+
+
+if getattr(sys, 'frozen', False):
+    current_directory = sys._MEIPASS
+else:
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+
+print(current_directory)
+
+monitored_key = e.get_hotKey_swap()
+hook_active = True
+swapper = "swapper.ps1"  # "Audio_switch.exe"
+last_alt_time = 0
+inform = False
+
+
+def toggle_hook():
+    global hook_active
+    hook_active = not hook_active
+
+
+def locate_file(swapper, current_directory):
+    for root, directory, files in os.walk(current_directory):
+        if swapper in files:
+            return os.path.join(root, swapper)
+    return None
+
+
+# class ThemeSwitcher:
+#     def __init__(self):
+#         self.switch = False
+#         self.themeSwitcher_path = locate_file(
+#             'ThemeSwitcher.exe', current_directory)
+#         self.theme_path_EN = 'C:\\Users\\oz\\AppData\\Roaming\\Microsoft\\Windows\\Themes\\Custom_EN.theme'
+#         self.theme_path_CZ = 'C:\\Users\\oz\\AppData\\Roaming\\Microsoft\\Windows\\Themes\\Custom_CZ.theme'
+
+#     def switch_theme(self):
+#         if self.switch:
+#             self.switch = False
+#             return self.theme_path_CZ
+#         else:
+#             self.switch = True
+#             return self.theme_path_EN
+
+
+# theme_switcher = ThemeSwitcher()
+
+
+def on_key_event(keyboard_event):
+    global hook_active
+    global last_alt_time
+    if hook_active:
+        if inform:
+            print(f"Pressed key:" + keyboard_event.name)
+        if keyboard_event.name == monitored_key:
+            print(f"{monitored_key} was pressed, executing powershell script...")
+            try:
+                # exe_path = locate_file(swapper, current_directory)
+                swapper_path = locate_file(swapper, current_directory)
+                if swapper_path is None:
+                    print("Exe not found")
+                else:
+                    print(f"PWS Swapper found on this path {swapper_path}")
+                    # sb.run([exe_path], shell=True)
+                    # script_path = f"{current_directory}\\swapper.ps1"
+                    sb.run(["powershell", "-File", swapper_path])
+                    # sb.run(exe_path)
+            except Exception as e:
+                print(f"An error occurred: {str(e)}")
+
+        if keyboard_event.name == 'alt':
+            last_alt_time = time.time()
+        elif keyboard_event.name == 'shift' and time.time() - last_alt_time < 0.5:
+            print("Combo 'left alt + left shift' pressed")
+
+            # sb.run([theme_switcher.themeSwitcher_path,
+            #        theme_switcher.switch_theme()])
+
+
+# Nastavení posluchače na zachycení kláves
+keyboard.on_press(on_key_event)
+
+# Pokud chcete zachytávat události klávesnice i mimo hlavní smyčku, můžete použít keyboard.wait()
+# keyboard.wait()
+print("Listening for key triggering")
+while True:
+    time.sleep(1)
---
+++ developed/Engine.py
@@ -0,0 +1,403 @@
+# !/usr/bin/env python
+# %%
+import subprocess as sb
+import re
+import ctypes
+import sys
+from dataclasses import dataclass, field
+import time
+import os
+import pickle
+import keyboard
+from enum import Enum
+
+class HotkeyType(Enum):
+    TRIGGER = 1
+    START_HOOK = 2
+    END_HOOK = 3
+
+@dataclass
+class SetupStuff:
+    result: str = None
+    confirm_list: set = ("y", "yes", "yeah", "ok", ".")
+    device_data: list = field(default_factory=list)
+    device_dict: dict = field(default_factory=dict)
+    selected_list: list = field(default_factory=list)
+    check: bool = None
+
+    def _confirm(self, value):
+        if value.lower() in self.confirm_list:
+            return True
+
+    def get_list(self, display_output=True):
+        self.result = sb.run(
+            ["powershell", "Get-AudioDevice -list"], capture_output=True, text=True)
+        if self.result.returncode == 0:
+            self.result = str(self.result.stdout.strip())
+            if display_output:
+                print(self.result)
+        return self.result
+
+    def convert_list(self):
+        """Converts powershell script to python readable format
+        """
+        # Použijeme regulární výraz k rozparsování vstupu
+        self.device_data = re.findall(
+            r'Index\s+: (\w+)\n?Default\s+: (\w+)\n?DefaultCommunication\s+: (\w+)\n?Type\s+: (.+)\n?Name\s+: (.+)\n?ID\s+: ({.+})\n?Device\s+: (.+)\n?', self.result)
+
+        for item in self.device_data:
+            index, default, default_communication, device_type, name, device_id, device = item
+            self.device_dict[int(index)] = {
+                'Default': default,
+                'DefaultCommunication': default_communication,
+                'Type': device_type.strip(),
+                'Name': name.strip(),
+                'ID': device_id.strip(),
+                'Device': device.strip()
+            }
+
+    def get_device_key(self, value, index):
+        """From parsed list returns values based on index of the item
+
+        Args:
+            value (str): Key for value. i.e. Name, ID
+            index (str): Index of the item, i.e. 1, 2, 5 
+
+        Raises:
+            TypeError: Prerequisites missing
+
+        Returns:
+            str: value of selected key and index of the parsed list
+        """
+        if self.device_dict is None:
+            raise TypeError(
+                "origin dictionary is not created, try use .get_list ->.convert_list first")
+
+        device_info = self.device_dict.get(index)
+        if device_info is not None:
+            return device_info[value]
+        else:
+            return None
+
+    def add_device(self, *index):
+        """Add devices to selected_list for export to powershell execution file
+
+        Raises:
+            ValueError: list can contain only 2 items at time
+
+        Returns:
+            list: list of selected items choosen for export to pws
+        """
+        self.get_list(False)
+        self.convert_list()
+        for i in index:
+            device_info = self.get_device_key("ID", i)
+            if len(self.selected_list) < 2:
+                self.selected_list.append(device_info)
+            else:
+                raise ValueError(
+                    f"{self.get_device_key('Name',i)} cannot be added. Your list of selected items is full")
+        return self.selected_list
+
+    def remove_device(self, position):
+        """Remove device from the selected_list. 0,1 choose position to delete or 2 to delete all
+
+        Args:
+            position (int): position of the item in a list you want to remove starting from 0
+
+        Returns:
+            list: list of selected items choosen for export to pws
+        """
+        if position == 2:
+            self.selected_list.clear()
+        else:
+            del self.selected_list[position]
+        return self.selected_list
+
+    def set_hotKey(self, hotkey_type, key):
+        print(f"Press the key you wish to save for {hotkey_type.name}: ")
+        inp_key_str = keyboard.read_key()
+        print(f"Your key is: {inp_key_str}")
+        with open(f"{os.path.dirname(__file__)}\\settings.pkl", 'wb') as f:
+            settings = {
+                HotkeyType.TRIGGER: None,
+                HotkeyType.START_HOOK: None,
+                HotkeyType.END_HOOK: None
+            }
+            settings[hotkey_type] = inp_key_str
+            pickle.dump(settings, f)
+            print(f"{inp_key_str} exported and saved as type: {hotkey_type.name}")
+
+    def get_hotKey(self, hotkey_type, path=os.path.dirname(__file__)):
+        try:
+            with open(f"{path}\\settings.pkl", 'rb') as f:
+                settings = pickle.load(f)
+                inp_key_str = settings.get(hotkey_type)
+                if inp_key_str:
+                    print(f"Loaded key for {hotkey_type.name} is: {inp_key_str}")
+                return inp_key_str
+        except Exception as error:
+            print(error)
+
+
+    def is_module_installed(self):
+        """Check whenever the AudioDevice Module is already installed or not
+
+        Returns:
+        Bool : False -> Not installed, run install_preq
+        True -> Installed, you can proceed to get list and stuff
+        """
+        output = sb.run(["powershell", "Get-InstalledModule"],
+                        capture_output=True, text=True)
+        print(output.stdout)
+        if len(re.findall('AudioDeviceCmdlets', output.stdout)) > 0:
+            self.check = True
+            print("Audio Module is installed")
+        else:
+            self.check = False
+            print("Audio Module is not Installed")
+
+    def pws_command(self, command, admin=False):
+        if admin == True:
+            result = sb.run(["runas", "/user:Administrator"] +
+                            command, capture_output=True, text=True)
+        else:
+            result = sb.run(command, capture_output=True, text=True)
+        print(result)
+        return result
+
+    def get_script_policy(self):
+        command = ["powershell", "Get-ExecutionPolicy"]
+        self.pws_command(command)
+        options = """
+Restricted: Skripty nejsou povoleny a lze spouštět pouze příkazy.
+AllSigned: Skripty mohou být spuštěny pouze tehdy, pokud jsou podepsány důvěryhodným vydavatelem.
+RemoteSigned: Lokální skripty mohou být spuštěny bez podpisu, ale skripty stažené z internetu musí být podepsány důvěryhodným vydavatelem.
+Unrestricted: Všechny skripty mohou být spuštěny bez omezení."""
+        print(options)
+
+    def set_script_policy(self):
+        options = input("What type of script policy you would like to choose")
+        match options:
+            case "Restricted":
+                print("Restricted policy selected")
+            case "AllSigned":
+                print("AllSigned policy selected")
+            case "RemoteSigned":
+                print("Remote Signed policy selected")
+            case "Unrestricted":
+                print("Unrestricted  policy selected")
+            case _:
+                print("Please select one of the policies")
+
+        command = ["powershell",
+                   f"Set-ExecutionPolicy {options} -Scope CurrentUser -Force"]
+
+        result = self.pws_command(command)
+        if (result.stderr or result.stdout) is (None or ""):
+            print(f"Policy changed to '{options}'")
+        else:
+            print(f"Something has broke {result.stderr},{result.stdout}")
+
+    def install_preq(self):
+        """Install the powershell module AudioDeviceCmdlets which allows manipulation with audiodevices.
+        Install Module -> Set Script Policy to RemoteSigned -> Converts list to Python readable format
+
+        Raises:
+            ValueError: _description_
+        """
+        self.is_module_installed()
+        if self.check is None:
+            raise ValueError(
+                "First you need to check if the module is installed or not, run 'is_module_installed'")
+        elif self.check == False:
+            try:
+                print("Installing ...")
+                command = [
+                    "powershell", "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force"]
+                self.pws_command(command)
+                print("Installing ... 17%")
+                command = [
+                    "powershell", "Install-Module -Name AudioDeviceCmdlets -Force -Scope CurrentUser"]
+                self.pws_command(command)
+                print("Installing ... 35%")
+                command = ["powershell",
+                           "Install-Module ps2exe -Force -Scope CurrentUser"]
+                self.pws_command(command)
+                print("Installing ... 48%")
+                self.get_list(False)
+                print("Installing ... 68%")
+                self.convert_list()
+                print("Installing ... 100%")
+            except Exception as error:
+                print(error)
+            finally:
+                self.is_module_installed()
+        else:
+            print("Module is already installed you can proceed to other operations")
+
+    def uninstall_preq(self):
+        inp = input(
+            "Do you want to uninstall Powershell AudioDevice Module (y/n): ")
+        if self._confirm(inp):
+            try:
+                command = ["powershell",
+                           "Uninstall-Module -Name AudioDeviceCmdlets -Force"]
+                self.pws_command(command)
+                command = ["powershell", "Uninstall-Module ps2exe -Force"]
+                self.pws_command(command)
+            except Exception as error:
+                print(error)
+        self.is_module_installed()
+        if self.check == False:
+            print("Module was sucesfully uninstalled")
+
+    def generate_pws_exe(self):
+        """ Generate a pws file which is execution script for device swap
+        """
+        if len(self.selected_list) == 2:
+            output_file = "swapper.ps1"
+            headset = self.selected_list[0]
+            speakers = self.selected_list[1]
+
+            # Vytvořte text skriptu
+            script_text = f"""\
+            ###### Swapper.Core #######
+            $headset = '{headset}'
+            $speakers = '{speakers}'
+
+            $headset_id = Get-AudioDevice -id $headset
+
+            if ($headset_id.default) {{
+                Set-AudioDevice -id $speakers
+                Write-Output 'Speakers Active'
+            }} else {{
+                Set-AudioDevice -id $headset
+                Write-Output 'Headset Active'
+            }}
+            """
+
+            with open(output_file, "w") as file:
+                file.write(script_text)
+
+            print(f"Soubor {output_file} byl vytvořen.")
+
+            self._convert_to_exe()
+        else:
+            raise ValueError(
+                f"{self.selected_list} does not contain 2 items. Check your selected list")
+
+    def _convert_to_exe(self):
+        """Converts ps1 script to executable
+
+        Raises:
+            NameError: Module ps2exe is not installed and is required to proceed
+        """
+        command = ["powershell", "Get-InstalledModule"]
+        result = self.pws_command(command)
+        if len(re.findall('ps2exe', result.stdout)) < 0:
+            raise NameError("ps2exe module is not installed, run install_preq")
+        current_directory = os.path.dirname(__file__)
+        print(current_directory)
+        try:
+            command = [
+                "powershell", f"Invoke-PS2EXE '{current_directory}\swapper.ps1' '{current_directory}\SuperSwapper.exe'"]
+            result = self.pws_command(command)
+        except Exception as error:
+            print(error)
+        else:
+            if (result.stderr or result.stdout == '') or result.returncode == 0:
+                print("File was converted to .exe sucessfully")
+            else:
+                print(
+                    f"Somthing has gone wrong in conversion\n{result.stderr},{result.stdout}")
+
+    def run_as_admin(self):
+        """Run a function as an admin
+        """
+        if sys.platform.startswith('win'):
+            try:
+                ctypes.windll.shell32.ShellExecuteW(
+                    None, "runas", sys.executable, __file__, None, 1)
+            except ctypes.WinError:
+                # Uživatel zrušil spuštění jako správce nebo se nepodařilo získat potřebná oprávnění
+                print(
+                    "User declined to start as Administrator or he/she lacks necessery permissions")
+                pass
+        else:
+            # Skript není spuštěn na platformě Windows
+            print("Script is not designed to run on other OS then Windows")
+            pass
+
+
+# e = SetupStuff()
+# e.set_hotKey_swap()
+# e.get_list()
+# e.convert_list()
+# e.install_preq()
+# e.uninstall_preq()
+# e.is_module_installed()
+print("Init done")
+
+
+if __name__ == "__main__":
+    audio_manager = SetupStuff()
+    audio_manager.run_as_admin()
+
+    print("Welcome to the Audio Setup Stuff program!")
+    print("================================")
+    print("To select an option, use the corresponding number below.")
+    print("If you are here for the first time start with Install option 6.")
+    print("Then Get the Audio Device List and locate desired devices for switch")
+    print("Memorize just its ID and add it to selected list with option 2")
+    print("Select your Activation hotkey for switch with option 8")
+    print("Finally Generate the executable script with option 5")
+    print("================================")
+    print("In second phase just start the newbord Audio_switch.exe")
+    print("Once you press the hotkey your output should change accordingly")
+    print("================================")
+
+    while True:
+        input("Press any key to continue: ")
+        print("\nAudio Device Manager Menu:")
+        print("1. Get Audio Device List")
+        print("2. Add Audio Device to Selected List")
+        print("3. Remove Audio Device from Selected List")
+        print("4. Show Selected list")
+        print("5. Generate PowerShell/Exe Script")
+        print("6. Install PowerShell Modules")
+        print("7. Uninstall PowerShell Modules")
+        print("8. Set the activation Hotkey")
+        print("9. Exit")
+
+        choice = input("Enter your choice: ")
+
+        match choice:
+            case "1":
+                audio_manager.get_list()
+                audio_manager.convert_list()
+                print("Audio Device List retrieved.")
+            case "2":
+                indices = input(
+                    "Enter the IDs of the audio devices to add (comma-separated): ").split(",")
+                audio_manager.add_device(*map(int, indices))
+            case "3":
+                position = int(input(
+                    "Enter the position of the audio device to remove (0 for first, 1 for second, 2 for all): "))
+                audio_manager.remove_device(position)
+            case "4":
+                print(audio_manager.selected_list)
+            case "5":
+                audio_manager.generate_pws_exe()
+            case "6":
+                audio_manager.install_preq()
+            case "7":
+                audio_manager.uninstall_preq()
+            case "8":
+                time.sleep(0.5)
+                audio_manager.set_hotKey_swap()
+            case "9":
+                print("Exiting...")
+                break
+            case _:
+                print("Invalid choice. Please select a valid option.")
